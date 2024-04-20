691e3c787193aba020d23ed94a2799b2d1a6c8ef
itsvishwa <116142492+itsvishwa@users.noreply.github.com>
2024-04-20 23:29:19 +0530

    doctor diagnosed form completed

--- client/src/components/AddDiagnose.tsx
+++ client/src/components/AddDiagnose.tsx
@@ -1,16 +1,49 @@
+import { useState } from "react";
 import DoctorForm from "./DoctorForm";
+import apiClient from "../services/api_client";
 
 function AddDiagnose() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isInvalid, setInvalid] = useState(false);
+  const [isVerified, setVerified] = useState(false);
+  const [_isLoading, _setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  const patientVerifyBtnHandler = (nic: String) => {
+    _setIsLoading(true);
+    setIsLoading(true);
+    apiClient
+      .get("/user/verifyPatient/?nic=" + nic)
+      .then((res) => {
+        setName(res.data.name);
+        setAge(res.data.age);
+        _setIsLoading(false);
+        setVerified(true);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        _setIsLoading(false);
+        setInvalid(true);
+        setIsLoading(false);
+      });
+  };
+
   return (
-    <div>
-      <DoctorForm
-        isLoading={true}
-        isInvalid={false}
-        isVerified={false}
-        name={"Vishwa Sandaruwan"}
-        age={"24"}
-      />
-    </div>
+    <>
+      <p className="mx-16 mt-8 text-xl font-medium">Add Patient Data</p>
+      <div className="mx-16 mt-4">
+        <DoctorForm
+          isLoading={isLoading}
+          isInvalid={isInvalid}
+          isVerified={isVerified}
+          name={name}
+          age={age}
+          patientVerifyBtnHandler={patientVerifyBtnHandler}
+        />
+      </div>
+    </>
   );
 }
 
---
+++ client/src/components/Loading.tsx
@@ -0,0 +1,10 @@
+function Loading() {
+  return (
+    <div className="mt-80 flex flex-col items-center align-middle">
+      <span className="loading loading-infinity loading-lg"></span>
+      <span>Loading</span>
+    </div>
+  );
+}
+
+export default Loading;
